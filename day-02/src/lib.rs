380566783689c57f78b4520e1ee676bccd38b257
Sebastian Luzynski <sebastian.luzynski@gmail.com>
2024-12-03 14:31:26 +0100

    day 02 part 2 solution

--- day-02/src/lib.rs
+++ day-02/src/lib.rs
@@ -1,52 +1,49 @@
 pub fn count_safe_reports(lines: Vec<String>) -> i32 {
     let numbers = convert(lines);
+    numbers
+        .iter()
+        .map(|row| verify_row(row))
+        .filter(|r| *r)
+        .count() as i32
+}
+
+pub fn count_safe_reports_with_reverification(lines: Vec<String>) -> i32 {
+    let numbers = convert(lines);
     let mut safe_reports = 0;
 
     for row in numbers {
-        let mut state = State::UNKNOWN;
-        let len = row.len();
-        let mut safe = true;
-
-        for i in 0..len - 1 {
-            let diff = row[i] - row[i + 1];
-            if diff == 0 {
-                safe = false;
-                break;
-            }
-            if i32::abs(diff) > 3 {
-                safe = false;
-                break;
-            }
-
-            match state {
-                State::INCREASING => {
-                    if diff > 0 {
-                        safe = false;
-                        break;
-                    }
-                }
-                State::DECREASING => {
-                    if diff < 0 {
-                        safe = false;
-                        break;
-                    }
-                }
-                State::UNKNOWN => {
-                    state = if diff > 0 {
-                        State::DECREASING
-                    } else {
-                        State::INCREASING
-                    };
-                }
-            }
-        }
-
-        if safe {
+        if verify_row(&row) {
             safe_reports += 1;
+        } else {
+            for i in 0..row.len() {
+                let mut modified = row.clone();
+                modified.remove(i);
+                if verify_row(&modified) {
+                    safe_reports += 1;
+                    break;
+                }
+            }
         }
     }
 
     safe_reports
+}
+
+fn verify_row(row: &Vec<i32>) -> bool {
+    let diffs = row
+        .as_slice()
+        .windows(2)
+        .map(|s| s[0] - s[1])
+        .collect::<Vec<i32>>();
+    if diffs.iter().any(|n| *n == 0 || i32::abs(*n) > 3) {
+        return false;
+    }
+
+    if diffs.iter().all(|n| *n < 0) || diffs.iter().all(|n| *n > 0) {
+        return true;
+    }
+
+    false
 }
 
 fn convert(lines: Vec<String>) -> Vec<Vec<i32>> {
@@ -60,20 +57,20 @@ fn convert(lines: Vec<String>) -> Vec<Vec<i32>> {
         .collect::<Vec<Vec<i32>>>()
 }
 
-enum State {
-    INCREASING,
-    DECREASING,
-    UNKNOWN,
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use utils::read_file;
 
     #[test]
-    fn basic_example() {
+    fn basic_example_part1() {
         let lines = read_file("test_input.txt").unwrap();
         assert_eq!(count_safe_reports(lines), 2);
+    }
+
+    #[test]
+    fn basic_example_part2() {
+        let lines = read_file("test_input.txt").unwrap();
+        assert_eq!(count_safe_reports_with_reverification(lines), 4);
     }
 }
--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,7 +1,7 @@
-use day_02::count_safe_reports;
+use day_02::count_safe_reports_with_reverification;
 use utils::read_file;
 
 fn main() {
     let lines = read_file("input.txt").unwrap();
-    println!("{}", count_safe_reports(lines));
+    println!("{}", count_safe_reports_with_reverification(lines));
 }
